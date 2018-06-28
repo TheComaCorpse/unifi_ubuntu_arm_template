4212743168c4f925a93b284042dc9226f9acde0b
Alexander Comas <acomas@sailormen.com>
2018-06-28 19:35:20 -0400

    debugging base deployment files

--- deploy/azuredeploy.ps1
+++ deploy/azuredeploy.ps1
@@ -1,8 +1,16 @@
+Connect-AzureRmAccount
+
+Select-AzureRmSubscription -SubscriptionId 03b5540a-3a3d-45b4-a3b8-d1432ffee710
+
 $templatefile = 'C:\Azure\ubiquiti_deploy\deploy\azuredeploy.json'
 $parameterfile = 'C:\Azure\ubiquiti_deploy\deploy\azuredeployparameters.json'
 
-Connect-AzureRmAccount
+
 
 New-AzureRmResourceGroup -Location 'East Us 2' -Name ComaCorpUnifi
 
-New-AzureRmResourceGroupDeployment -Name Unifideploy -ResourceGroupName ComaCorpUnifi -TemplateFile $templatefile -TemplateParameterFile $parameterfile -verbose
+New-AzureRmResourceGroupDeployment -Name Unifideploy -ResourceGroupName ComaCorpUnifi `
+                                   -TemplateFile $templatefile `
+                                   -TemplateParameterFile $parameterfile `
+                                   -verbose `
+                                   -DeploymentDebugLogLevel All
