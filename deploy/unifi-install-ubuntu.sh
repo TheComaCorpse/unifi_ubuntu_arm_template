62085f686322a2adec2a95c42047db8b95da20b7
TheComaCorpse <31578630+TheComaCorpse@users.noreply.github.com>
2018-07-01 12:55:47 -0400

    updated and added comments to unifi-install-ubuntu.sh

--- deploy/unifi-install-ubuntu.sh
+++ deploy/unifi-install-ubuntu.sh
@@ -1,8 +1,11 @@
 #install Ubiquiti unifi controller
-echo 'deb http://www.ubnt.com/downloads/unifi/debian stable ubiquiti' | sudo tee /etc/apt/sources.list.d/100-ubnt.unifi.list
+echo 'deb http://www.ubnt.com/downloads/unifi/debian stable ubiquiti' | sudo tee /etc/apt/sources.list.d/100-ubnt-unifi.list
 
-sudo wget -0 /etc/apt/trusted.gpg.d/unifi-repo-gpg https://dl.ubnt.com/unifi/unifi-repo.gpg
+#add GPG Keys
+sudo wget -O /etc/apt/trusted.gpg.d/unifi-repo.gpg https://dl.ubnt.com/unifi/unifi-repo.gpg 
 
+#Update to latest Package
 sudo apt-get update --allow-releaseinfo-change
 
+#Install and upgrade the Unifi Controller
 sudo apt-get install unifi
