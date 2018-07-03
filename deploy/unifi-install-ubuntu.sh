7df33d91efb3a5e98f2503d2890fe5e3f1c93015
Alexander Comas <acomas@sailormen.com>
2018-07-03 13:54:13 -0400

    moved openjdk8 to own line add allow unauthenticated

--- deploy/unifi-install-ubuntu.sh
+++ deploy/unifi-install-ubuntu.sh
@@ -2,11 +2,14 @@
 echo 'deb http://www.ubnt.com/downloads/unifi/debian stable ubiquiti' | sudo tee -a /etc/apt/sources.list
 
 #add GPG Keys
-sudo apt-key -y adv --keyserver keyserver.ubuntu.com --recv 06E85760C0A52C50
+sudo apt-key -y adv --keyserver keyserver.ubuntu.com --recv C0A52C50
 
 #Update to latest Package
 sudo apt-get -y update 
 
 #Install and upgrade the Unifi Controller and Java SDK 8
 sudo apt-get -y install unifi openjdk-8-jdk
+
+#Install Java 8 SDK 
+sudo apt-get -y install openjdk-8-jdk --allow-unauthenticated
 
