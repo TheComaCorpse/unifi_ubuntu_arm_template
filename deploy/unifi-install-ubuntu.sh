d33c3169dcc3df39e6aa086ab3aed54313fa8e4a
Alexander Comas <acomas@sailormen.com>
2018-07-03 13:27:59 -0400

    updated sudo apt-get

--- deploy/unifi-install-ubuntu.sh
+++ deploy/unifi-install-ubuntu.sh
@@ -5,7 +5,7 @@ echo 'deb http://www.ubnt.com/downloads/unifi/debian stable ubiquiti' | sudo tee
 sudo wget -O /etc/apt/trusted.gpg.d/unifi-repo.gpg https://dl.ubnt.com/unifi/unifi-repo.gpg 
 
 #Update to latest Package
-sudo apt-get update --allow-releaseinfo-change
+sudo apt-get -y update 
 
 #Install and upgrade the Unifi Controller and Java SDK 8
 sudo apt-get -y install unifi openjdk-8-jdk
