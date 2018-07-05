1b4ed7c266e95c324fa8f7e4e49d67ad4dfcf347
Alexander Comas <acomas@sailormen.com>
2018-07-05 11:13:06 -0400

    update install packages

--- deploy/unifi-install-ubuntu.sh
+++ deploy/unifi-install-ubuntu.sh
@@ -2,15 +2,17 @@
 echo 'deb http://www.ubnt.com/downloads/unifi/debian stable ubiquiti' | sudo tee -a /etc/apt/sources.list
 
 #add GPG Keys
-sudo apt-key -y --allow-unauthenticated adv --keyserver keyserver.ubuntu.com --recv 06E85760C0A52C50  
-sudo apt-key -y --allow-unauthenticated adv --keyserver keyserver.ubuntu.com -recv 7f0ceb10
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv 06E85760C0A52C50  
+sudo apt-key adv --keyserver keyserver.ubuntu.com -recv A15703C6
 
 #Update to latest Package
 sudo apt-get -y --allow-unauthenticated update 
 
+
+#Install Java 8 SDK 
+sudo apt-get -y --allow-unauthenticated install openjdk-8-jdk 
+
 #Install and upgrade the Unifi Controller and Java SDK 8
 sudo apt-get -y --allow-unauthenticated install Unifi
 
-#Install Java 8 SDK 
-#sudo apt-get -y install openjdk-8-jdk --allow-unauthenticated
 
